96fbf2ac45b0fe64dbde3d1bda85080ceab6afb7
Lucky Cungwa <36919645+luckycungwa@users.noreply.github.com>
2023-07-10 08:01:49 +0300

    form ui edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import Login from './comp/Login';
+import "./App.css";
+import Login from "./comp/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <>
+      <div className="App">
+        {/* header section */}
+        <div className="header dark-bg">1</div>
+
+        {/* Main body section */}
+        <div className="main">
+          <Login />
+        </div>
+
+        {/* Footer Section*/}
+        <div className="footer dark-bg">2</div>
+      </div>
+    </>
   );
 }
 
--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -4,21 +4,43 @@ import React from "react";
 function Login() {
   return (
     <>
-      <div className="">
-        <form>
+      <div className="form-section glass">
+        {/* <div className="">
+          <img className="card-img" src="./login.jpg" alt="signup" />
+        </div> */}
         <div className="form-field">
-            <label for="fname">EMAIL ADDRESS</label>
-            <br/>
-          <input type="text" id="fname" name="fname" />
-        </div>
-          
-          <br/>
+          <div class="form-field">
+            <label class="" for="textinput">
+              Username
+            </label>
+            <br />
+            <div className="">
+              <input type="text" placeholder="Username" />
+            </div>
+          </div>
+          <br />
+          <div class="form-field">
+            <label class="" for="textinput">
+              Password
+            </label>
+            <br />
+            <div className="">
+              <input type="password" placeholder="password" />
+            </div>
+          </div>
+          <br />
 
-          <label for="lname">PASSWORD</label>
-          <input type="text" id="lname" name="lname" />
-          <br/>
-          <button className="btn primary">LOGIN</button>
-        </form>
+          <div>
+            <button className="btn cta primary">LOGIN</button>
+          </div>
+        </div>
+        <div className="image card-img">
+          <img className="card-img" src="./login.jpg" alt="signup" />
+          <div className="card-info">
+            <p className="text-1">LOG IN</p>
+            <p className="text-1">WELCOME TO OUR HOTEL</p>
+          </div>
+        </div>
       </div>
     </>
   );
