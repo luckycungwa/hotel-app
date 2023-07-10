5c28b8d3c083ce8e2aaf6b7750a7f637fe2491e7
luckycungwa <cungwalucky@gmail.com>
2023-07-10 09:44:27 +0200

    more for edits

--- src/comp/Login.jsx
+++ src/comp/Login.jsx
@@ -1,21 +1,73 @@
-import "./css/form.css";
 import React from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../config/firebase";
 
 function Login() {
+  //const [userInput, setUserInput] = useState("");
+  const [userName, setUserName] = useState(""); //change defaulkts to match info from the database/
+  const [userPass, setUserPass] = useState(""); //change defaulkts to match info from the database/authentication etc.
+
+  //Prevent user from submiting an empty form
+  const ForceInput = (e) => {
+    if (!userName) {
+      alert("You must add at least 1 task!"); // Alert the user if they did not filll in the task
+      return;
+    }
+  };
+
+  //user registeration & authentication
+  const handleLogin = () => {
+    ForceInput();
+
+    const authUser = getAuth();
+
+    signInWithEmailAndPassword(authUser, userName, userPass)
+      .then(() => {
+        alert("Login successful!");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("An error occurred. Please try again.");
+      });
+  };
+
   return (
     <>
-      <div className="">
-        <form>
+      <div className="main">
+        <form className="form">
         <div className="form-field">
             <label for="fname">EMAIL ADDRESS</label>
             <br/>
-          <input type="text" id="fname" name="fname" />
+            <input
+                  autoFocus
+                  className=""
+                  placeholder="username or email"
+                  type="text/email"
+                  autoComplete="on"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  // force user to fill force
+                  required
+                />
         </div>
           
           <br/>
 
           <label for="lname">PASSWORD</label>
-          <input type="text" id="lname" name="lname" />
+          <input
+                  className=""
+                  placeholder="password"
+                  type="password"
+                  value={userPass}
+                  required
+                  autoComplete="off"
+                  //Disable handler until we complete Firebase Lessons
+                  onChange={(e) => setUserPass(e.target.value)}
+                  // force user to fill force
+                />
           <br/>
           <button className="btn primary">LOGIN</button>
         </form>
